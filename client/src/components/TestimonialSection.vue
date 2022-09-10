8e9f1fb1a1286fb967b57544708608d864997bc9
KamCy Ahaka <69519905+Kilo0110@users.noreply.github.com>
2022-09-10 17:06:52 -0600

    Updated Home view (#2)

---
+++ client/src/components/TestimonialSection.vue
@@ -0,0 +1,136 @@
+<template>
+  <section class="section testimonial-section d-flex flex-column align-center">
+    <h1 class="text-center">What People Are Saying About Us</h1>
+    <div class="carousel" data-carousel>
+      <button class="carousel__btn carousel__btn--prev">&lArr;</button>
+      <button class="carousel__btn carousel__btn--next">&rArr;</button>
+      <ul class="testimonial-container">
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+          data-active
+        >
+          <img src="img/team/male chef 1.jpg" alt="" class="testifier-image" />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+        >
+          <img
+            src="img/team/female chef 1.jpeg"
+            alt=""
+            class="testifier-image"
+          />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+        >
+          <img src="img/team/manager 1.jpg" alt="" class="testifier-image" />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+        >
+          <img src="img/team/owner.jpeg" alt="" class="testifier-image" />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+        >
+          <img
+            src="img/team/delivery man 1.jpg"
+            alt=""
+            class="testifier-image"
+          />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+        <li
+          class="testimonial testimonial-slide d-flex flex-column align-center jfy-around"
+        >
+          <img src="img/team/waiter 1.jpeg" alt="" class="testifier-image" />
+
+          <div class="testimonial__content-container">
+            <i class="fa-solid fa-quote-left"></i>
+            <i class="fa-solid fa-quote-right"></i>
+            <div class="testimonial__content text-center">
+              <h2 class="testifier-name">Lorem, ipsum.</h2>
+              <p class="testimony">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                minus perspiciatis excepturi. Esse, pariatur libero quod nihil
+                recusandae blanditiis optio!
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TestimonialSection",
+};
+</script>
+
+<style lang="scss"></style>
