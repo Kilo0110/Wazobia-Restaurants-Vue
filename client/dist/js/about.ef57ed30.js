b2281b7c0b927735198103e43bedfa37fcf19913
KamCy Ahaka <69519905+Kilo0110@users.noreply.github.com>
2022-09-10 17:28:08 -0600

    Dev (#4)
    * Updated Home view
    
    * Built files for production (#3)

---
+++ client/dist/js/about.ef57ed30.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{3725:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var u=e(7875);const a={class:"about"},c=(0,u._)("h1",null,"This is an about page",-1),r=[c];function s(n,t){return(0,u.wg)(),(0,u.iD)("div",a,r)}var i=e(1935);const l={},o=(0,i.Z)(l,[["render",s]]);var f=o}}]);
+//# sourceMappingURL=about.ef57ed30.js.map
