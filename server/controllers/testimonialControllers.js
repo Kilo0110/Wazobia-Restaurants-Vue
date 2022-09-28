59328066f4ccb6f971424559651ffed4e6de2044
KamCy Ahaka <kamcyahaka@gmail.com>
2022-09-28 22:43:40 +0100

    Updated serverside code
    > Updated logic for testimonials
    > Added userModel and routes
    > Updated mealModel to have a price type of string
    > Added authentication middleware to be used for restricted routes

--- server/controllers/testimonialControllers.js
+++ server/controllers/testimonialControllers.js
@@ -8,7 +8,7 @@ const Testimonial = require("../models/testimonialModel");
 // @access Private
 const getTestimonials = asyncHandler(async (req, res) => {
   let testimonials
-  testimonials = await Testimonial.find({}).limit(5)
+  testimonials = await Testimonial.find({ user: req.user.id }).limit(5)
 
   if (Object.keys(req.query).length !== 0) {
     let amountToGet = req.query.amount
@@ -19,9 +19,84 @@ const getTestimonials = asyncHandler(async (req, res) => {
   res.status(200).json(testimonials)
 });
 
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
+const setTestimonial = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
+  const testimonial = await Testimonial.create({
+    text: req.body.text,
+    user: req.user.id,
+  })
+
+  res.status(200).json(testimonial)
+})
+
+// @desc    Update testimonial
+// @route   PUT /api/testimonials/:id
+// @access  Private
+const updateTestimonial = asyncHandler(async (req, res) => {
+  const testimonial = await Testimonial.findById(req.params.id)
+
+  if (!testimonial) {
+    res.status(400)
+    throw new Error('Testimonial not found')
+  }
+
+  // Check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // Make sure the logged in user matches the testimonial user
+  if (testimonial.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  const updatedTestimonial = await Testimonial.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedTestimonial)
+})
+
+// @desc    Delete testimonial
+// @route   DELETE /api/testimonials/:id
+// @access  Private
+const deleteTestimonial = asyncHandler(async (req, res) => {
+  const testimonial = await Testimonial.findById(req.params.id)
+
+  if (!testimonial) {
+    res.status(400)
+    throw new Error('Testimonial not found')
+  }
+
+  // Check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // Make sure the logged in user matches the testimonial user
+  if (testimonial.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  await testimonial.remove()
+
+  res.status(200).json({ id: req.params.id })
+})
+
 module.exports = {
   getTestimonials,
-  // setMeal,
-  // updateMeal,
-  // deleteMeal,
+  setTestimonial,
+  updateTestimonial,
+  deleteTestimonial,
 };
