3a1a742db423e8967e7488d7c719bae35403b53a
KamCy Ahaka <kamcyahaka@gmail.com>
2022-09-28 22:40:07 +0100

    Included authentication module for vuex
    Also set axios to make requests with credentials in main.js file

--- client/src/main.js
+++ client/src/main.js
@@ -3,5 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:3000/api";
 
 createApp(App).use(store).use(router).mount("#app");
