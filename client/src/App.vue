ae2b30d3d6cd7df15ccd91ef1383cbb026e37d6e
KamCy Ahaka <kamcyahaka@gmail.com>
2022-09-28 22:44:02 +0100

    Removed API call in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,3 @@
 <template>
   <router-view></router-view>
 </template>
-
-<script>
-import axios from "axios";
-
-export default {
-  name: "App",
-  data() {
-    return {
-      todos: [],
-      description: "",
-      name: "",
-    };
-  },
-  async mounted() {
-    const response = await axios.get("/api/meals");
-    this.todos = response.data;
-  },
-  methods: {
-    async addTodo(e) {
-      e.preventDefault();
-      const response = await axios.post("api/meals", {
-        name: this.name,
-        description: this.description,
-      });
-      this.todos.push(response.data);
-      this.name = "";
-      this.description = "";
-    },
-    async removeTodo(item, i) {
-      await axios.delete("api/meals" + item._id);
-      this.todos.splice(i, 1);
-    },
-  },
-};
-</script>
